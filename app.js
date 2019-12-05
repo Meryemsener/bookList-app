c63939abbbf5e0590269a2ead89b13108750e87d
Meryem Sener <meryemaydinsener@gmail.com>
2019-12-05 20:27:37 +0100

    add alerts

--- app.js
+++ app.js
@@ -38,9 +38,25 @@ class UI {
       <td>${book.isbn}</td> 
       <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
       `;
-list.appendChild(row);;
-
+list.appendChild(row);
   }
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+  
+  static showAlert(message, className){
+     const div = document.createElement('div');
+     div.className = 'alert-${className}';
+     div.appendChild(document.createTextNode(message));
+     const container = document.querySelector('.container');
+     const form = document.querySelector('#book-form');
+     container.insertBefore(div, form);
+    //3 second
+    setTimeout(()=>document.querySelector('.alert').remove(),3000);
+  }
+  
   static clearFields(){
       document.querySelector('#title').value = '';
       document.querySelector('#author').value = '';
@@ -63,12 +79,26 @@ document.querySelector('#book-form').addEventListener('submit', (e)=> {
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
-//instatiate book
-const book = new Book(title, author, isbn);
-//add book to UL
-UI.addBookToList(book);
-//clear
-UI.clearFields();
+//validate
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please fill in all blank", "danger");
+  } else {
+   
+    //instatiate book
+    const book = new Book(title, author, isbn);
+    //add book to UL
+    UI.addBookToList(book);
+    //Show success message
+    UI.showAlert('Book Added', 'success');
+    //clear
+    UI.clearFields();
+  }
 });
+  
 //event:remove a book*/
-
+document.querySelector("#book-list").addEventListener("click", (e)=> {
+  UI.deleteBook(e.target);
+  
+  //show success message
+    UI.showAlert('Book Removed', 'success');
+});
